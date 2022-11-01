ac3cfd9698b8719d5a2d97e885ec7c56e618ae23
mailbadirov <mailbadirov@gmail.com>
2022-11-01 22:20:41 +0300

    hotfix

--- script.js
+++ script.js
@@ -5,15 +5,37 @@ const options = {
   second: '2-digit',
 };
 
-const startStopButton = document.querySelector('.start-stop-button');
-const roundButton = document.querySelector('.round-button');
-const clearButton = document.querySelector('.clear-button');
+const ELEMENT_CLASSES = {
+  START_STOP_BUTTON: '.start-stop-button',
+  ROUND_BUTTON: '.round-button',
+  CLEAR_BUTTON: '.clear-button',
+  TIMER: '.timer',
+  TIME: '.time',
+  ROUND_HISTORY: '.round-history',
+};
 
-const timerLine = document.querySelector('.timer');
-const timeLine = document.querySelector('.time');
-const roundHistory = document.querySelector('.round-history');
+const STRINGS = {
+  START_BUTTON: 'Start',
+  STOP_BUTTON: 'Stop',
+};
 
-let timerID;
+const NUMBERS = {
+  MIL_IN_SEC: 100,
+  SEC_IN_MIN: 60,
+  MIN_IN_HOUR: 60,
+};
+
+const startStopButton = document.querySelector(
+  ELEMENT_CLASSES.START_STOP_BUTTON
+);
+const roundButton = document.querySelector(ELEMENT_CLASSES.ROUND_BUTTON);
+const clearButton = document.querySelector(ELEMENT_CLASSES.CLEAR_BUTTON);
+
+const timerLine = document.querySelector(ELEMENT_CLASSES.TIMER);
+const timeLine = document.querySelector(ELEMENT_CLASSES.TIME);
+const roundHistory = document.querySelector(ELEMENT_CLASSES.ROUND_HISTORY);
+
+let timerId;
 let [timerMinutes, timerSeconds, timerMilliseconds, roundCount] = [0, 0, 0, 0];
 
 const addZeros = (value) => {
@@ -22,29 +44,33 @@ const addZeros = (value) => {
 
 const startStopTimer = (event) => {
   roundButton.disabled = !roundButton.disabled;
-  event.target.textContent = 'Stop';
 
-  if (timerID) {
-    event.target.textContent = 'Start';
-    clearInterval(timerID);
-    timerID = null;
+  event.target.textContent = STRINGS.STOP_BUTTON;
+
+  if (timerId) {
+    event.target.textContent = STRINGS.START_BUTTON;
+
+    clearInterval(timerId);
+
+    timerId = null;
+
     return;
   }
 
-  timerID = setInterval(() => {
-    timerMilliseconds += 1;
+  timerId = setInterval(() => {
+    timerMilliseconds++;
 
-    if (timerMilliseconds === 100) {
+    if (timerMilliseconds === NUMBERS.MIL_IN_SEC) {
       timerMilliseconds = 0;
       timerSeconds++;
     }
 
-    if (timerSeconds === 60) {
+    if (timerSeconds === NUMBERS.SEC_IN_MIN) {
       timerSeconds = 0;
       timerMinutes++;
     }
 
-    if (timerMinutes === 60) {
+    if (timerMinutes === NUMBERS.MIN_IN_HOUR) {
       timerMinutes = 0;
     }
 
@@ -58,12 +84,14 @@ startStopButton.addEventListener('click', startStopTimer);
 
 clearButton.addEventListener('click', () => {
   [roundCount, timerMinutes, timerSeconds, timerMilliseconds] = [0, 0, 0, 0];
+
   [timerLine.textContent, roundHistory.textContent] = ['00 : 00.00', ''];
+
   roundHistory.style.opacity = roundCount;
 });
 
 roundButton.addEventListener('click', () => {
-  if (timerID) {
+  if (timerId) {
     roundHistory.innerHTML =
       `#${++roundCount} ------ ${timerLine.textContent}<br>` +
       roundHistory.innerHTML;
@@ -74,6 +102,7 @@ roundButton.addEventListener('click', () => {
 
 const getTime = () => {
   const myDate = new Date();
+
   timeLine.textContent = myDate.toLocaleTimeString('en-us', options);
 };
 
